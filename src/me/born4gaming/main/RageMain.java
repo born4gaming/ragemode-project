b5a5e7b92e667555a02afa0d9b8694ac43d09bd2
Benedikt <Benedikt@Benedikt-PC.fritz.box>
2015-08-17 23:07:16 +0200

    Added RageMain as new main

---
+++ src/me/born4gaming/main/RageMain.java
@@ -0,0 +1,41 @@
+package me.born4gaming.main;
+
+
+import me.born4gaming.items.Items;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RageMain extends JavaPlugin {
+	
+	
+	
+	public void onEnable()
+	{
+		Bukkit.getPluginManager().registerEvents(new Items(), this);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
